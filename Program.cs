ef0774cf591d2fd1362282d410abdb4425aaef31
Diogo L Scarmagnani <diogols@live.com>
2022-09-16 10:07:30 -0300

    added Models

--- Program.cs
+++ Program.cs
@@ -1,75 +1,38 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SomethingInCSharp.Writer;
 
-namespace SomethingInCSharp
+internal class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        static void ClearCurrentConsoleLine()
-        {
-            int currentLineCursor = Console.CursorTop;
-            Thread.Sleep(100);
-            Console.SetCursorPosition(0, currentLineCursor);
-            Console.Write(new string(' ', Console.WindowWidth-1));
-            Thread.Sleep(100); 
-            Console.SetCursorPosition(0, currentLineCursor);
-        }
+        string header = "----- Programa de mensagens -----";
+        string bomdia = "Bom dia";
+        string loading = "Loading";
+        string ok = "Ok então...";
+        string msg = "" +
+            "---------- PARABÉNS :) ----------" +
+            "\n" +
+            "--- Feliz dia do programador! ---" +
+            "\n" +
+            "---------------------------------" +
+            "\n";
 
-        static void WriteNow(string bomdia, int sec, bool end)
-        {
-            for (int i = 0; i < bomdia.Length; i++) {
-                if (i == bomdia.Length-1 && end == true)
-                    Console.WriteLine(bomdia[i]);
-                else
-                    Console.Write(bomdia[i]);
-                Thread.Sleep(sec);
-            }
-        }
-        static void Blinker(string text)
-        {
-            int currentLineCursor = Console.CursorTop;
-            
-            for (int i = 0; i < 3; i++) {
-                Console.SetCursorPosition(0, currentLineCursor);
-                ClearCurrentConsoleLine();
-                WriteNow(text, 50, false);
-            }
-        }
+        Writer.WriteNow(header, 25, false);
+        Console.WriteLine();
 
-        public static void Main(string[] args)
-        {
-            string header = "----- Programa de mensagens -----";
-            string bomdia = "Bom dia";
-            string loading = "Loading";
-            string msg = ""+
-                "---------- PARABÉNS :) ----------"+
-                "\n"+
-                "--- Feliz dia do programador! ---"+
-                "\n"+
-                "---------------------------------"+
-                "\n";
+        Console.WriteLine();
+        Thread.Sleep(500);
+        Writer.WriteNow(bomdia + "!", 40, true);
+        Writer.WriteNow("Que dia é hoje? ", 40, false);
+        string dia = Console.ReadLine();
 
-            WriteNow(header, 25, false);
-            Console.WriteLine();
-
-            Console.WriteLine();
-            Thread.Sleep(500);
-            WriteNow(bomdia+"!", 40, true);
-            WriteNow("Que dia é hoje? ", 40, false);
-            string dia = Console.ReadLine();
-
-            if (dia == "13/09/2022") {
-                Console.WriteLine();
-                Blinker(loading+"...");
-                Thread.Sleep(800);
-                ClearCurrentConsoleLine();
-                Thread.Sleep(100);
-                WriteNow(msg, 25, false);
-            }
-        }
+        Console.WriteLine();
+        Writer.Blinker(loading + "...");
+        Thread.Sleep(800);
+        Writer.ClearCurrentConsoleLine();
+        Thread.Sleep(100);
+        if (dia == "13/09/2022")
+            Writer.WriteNow(msg, 25, false);
+        else
+            Writer.WriteNow(ok, 25, false);
     }
 }
